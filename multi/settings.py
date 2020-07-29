45250cffecc594636264b0bc3ee543923c8482de
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-29 10:32:49 +0530

    make

--- multi/settings.py
+++ multi/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECTROOT = os.path.dirname(_file)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '')
 STATIC_URL = '/'
 
